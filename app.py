c9919f5a6f45d65d109361e4e34476aa1c7d73bc
Drish <drish.xd@gmail.com>
2022-01-07 18:30:50 +0530

    init api

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, redirect
+import random, json
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return redirect("http://drish-xd.is-a.dev/")
+
+
+@app.route("/quotes")
+def quotes():
+    file = open("quotes.json", "r", encoding="utf8")
+    quotes = json.load(file)
+    return jsonify(quotes)
+    file.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=2000)
