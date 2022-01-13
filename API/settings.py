b617973d630b04a4f63d0cd6e0e7f03fc36bf822
Drish <drish.xd@gmail.com>
2022-01-13 17:18:35 +0530

    init flask app

--- API/settings.py
+++ API/settings.py
@@ -7,5 +7,5 @@ load_dotenv(dotenv_path)
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
