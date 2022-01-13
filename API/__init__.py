080e41e6eb008625991cebfbbf02b3fddafd1e4b
Drish <drish.xd@gmail.com>
2022-01-13 17:35:04 +0530

    init flask app

--- API/__init__.py
+++ API/__init__.py
@@ -10,7 +10,6 @@ from API.routes.quotes import quotes
 def create_app(config_file="settings.py"):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
-    print(app.config['SECRET_KEY'])
     db.init_app(app)
 
     app.register_blueprint(auth)
